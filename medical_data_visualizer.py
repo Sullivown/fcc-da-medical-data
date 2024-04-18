0b23ab399f3b9643e7ce23ee6252d43ec59d595b
Daniel Sullivan <daniel.w.t.sullivan@gmail.com>
2024-04-18 12:12:29 +0000

    refactor zero and one conversion into function

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -13,16 +13,19 @@ df['overweight'].replace({True: 1, False: 0}, inplace=True)
 
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
-df['gluc'][df['gluc'] == 1] = 0
-df['gluc'][df['gluc'] > 1] = 1
-df['cholesterol'][df['cholesterol'] == 1] = 0
-df['cholesterol'][df['cholesterol'] > 1] = 1
-print(df.head())
+def convert_to_zeros_ones(cell_data):
+    return 0 if cell_data == 1 else 1 
+
+df['cholesterol'] = df['cholesterol'].apply(convert_to_zeros_ones)
+df['gluc'] = df['gluc'].apply(convert_to_zeros_ones)
+
+print(df)
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = pd.melt(df, value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    # df_cat = pd.melt(df, value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    # print(df_cat)
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
