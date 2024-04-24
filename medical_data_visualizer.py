fix overweight formula and add catplot

medical_data_visualizer.py
@@ -5,11 +5,12 @@ import numpy as np
 
 # Import data
 df = pd.read_csv('./medical_examination.csv')
-print(df.head())
+# print(df.tail())
 
 # Add 'overweight' column
-df['overweight'] = df['weight'] / df['height'] ** 2 > 25
+df['overweight'] = (df['weight'] / np.square(df['height'] / 100)) > 25
 df['overweight'].replace({True: 1, False: 0}, inplace=True)
+# print(df['overweight'].value_counts())
 
 
 # Normalize data by making 0 always good and 1 always bad. If the value of 'cholesterol' or 'gluc' is 1, make the value 0. If the value is more than 1, make the value 1.
@@ -19,31 +20,44 @@ def convert_to_zeros_ones(cell_data):
 df['cholesterol'] = df['cholesterol'].apply(convert_to_zeros_ones)
 df['gluc'] = df['gluc'].apply(convert_to_zeros_ones)
 
-print(df)
+# print(df)
+
 
 # Draw Categorical Plot
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol', 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    # df_cat = pd.melt(df, value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
+    df_cat = pd.melt(df, id_vars=["cardio"], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
     # print(df_cat)
 
 
     # Group and reformat the data to split it by 'cardio'. Show the counts of each feature. You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    df_cat = df_cat.reset_index() \
+                .groupby(['variable', 'cardio', 'value']) \
+                .agg('count') \
+                .rename(columns={'index': 'total'}) \
+                .reset_index()
+    # print(df_cat)
 
     # Draw the catplot with 'sns.catplot()'
-
+    cardio_catplot = sns.catplot(
+        x="variable",
+        y="total",
+        col="cardio",
+        hue="value",
+        data=df_cat,
+        kind="bar")
 
 
     # Get the figure for the output
-    fig = None
+    fig = cardio_catplot.fig
 
 
     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
+
+draw_cat_plot()
 
 # Draw Heat Map
 def draw_heat_map():